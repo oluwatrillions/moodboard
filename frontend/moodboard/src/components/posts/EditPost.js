3d8b8937e1ab91a34614f86aaed882d39fc461de
Stephen Amade Reuben <streuby@live.com>
2022-08-24 17:59:08 +0100

    fixed burg with EditPost

--- frontend/moodboard/src/components/posts/EditPost.js
+++ frontend/moodboard/src/components/posts/EditPost.js
@@ -1,69 +1,93 @@
-import { Container, Typography } from '@material-ui/core'
-import axios from 'axios'
-import React, { useEffect, useRef, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { Container, Typography } from "@material-ui/core";
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
-const EditPost = ({editPost, feed, editMood, setEditMood, editTitle, setEditTitle}) => {
-    const { id } = useParams()
-    // const post = feed.filter(post => post.id.toString() !== id)
-    
-    const navigate = useNavigate()
-    const [editedTitle, setEditedTitle] = useState([])
-    const [editedMood, setEditedMood] = useState([])
-    const [moods, setMoods] = useState('')
+const EditPost = ({
+  editPost,
+  feed,
+  editMood,
+  setEditMood,
+  editTitle,
+  setEditTitle,
+}) => {
+  const { id } = useParams();
+  // const post = feed.filter(post => post.id.toString() !== id)
 
-    const errMsg = useRef()
-    const errRef = useRef()
+  const navigate = useNavigate();
+  const [editedTitle, setEditedTitle] = useState([]);
+  const [editedMood, setEditedMood] = useState([]);
+  const [moods, setMoods] = useState("");
 
-    // useEffect(() => {
-    //     if (post) {
-    //         setEditMood(post.mood)
-    //         setEditTitle(post.title)
-    //     }
-    // }, post, setEditTitle, setEditMood)
+  const errMsg = useRef();
+  const errRef = useRef();
 
-        const editedPost = async () => {
-            await axios.put(`http://localhost:4000/post/${id}`, {
-            }).then((response) => {
-                setEditedMood(response.data.mood)
-                setEditedTitle(response.data.title)
-            }).catch((err) => {
-                console.log(err);
-            })
-        }
-
+  const editedPost = async () => {
+    await axios
+      .put(`http://localhost:4000/post/${id}`, {})
+      .then((response) => {
+        setEditedMood(response.data.mood);
+        setEditedTitle(response.data.title);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <Container disableGutters maxWidth={false}>
-          <div className='parent'>
-              {/* { editTitle &&
+      <div className="parent">
+        {/* { editTitle &&
                 <> */}
-                  <Container disableGutters maxWidth={false} component='div' className='primary'>
-                      <section className='mood-section'>
-                          <Typography variant='h4' className='mood-heading'>What's your mood like today?</Typography>
-                          <Typography variant='h5' ref={errRef} className={errMsg ? 'errMessage' : 'noErrMessage'}>{errMsg}</Typography>
-                      
-                          <form >
-                              <label htmlFor='title'></label>
-                              <input type='text' name='title' value={editedTitle} onChange={(e) => (setEditTitle(e.target.value))} />
-                          
-                              <label htmlFor='mood'></label>
-                              <textarea name='name' rows={4} value={editedMood} onChange={(e) => (setEditMood(e.target.value))}></textarea>
-                          
-                          <button className='btn' onClick={editedPost}>UPDATE</button>
-                          </form>
-                      </section>
-                    </Container>
-                {/* </>
+        <Container
+          disableGutters
+          maxWidth={false}
+          component="div"
+          className="primary"
+        >
+          <section className="mood-section">
+            <Typography variant="h4" className="mood-heading">
+              What's your mood like today?
+            </Typography>
+            <Typography
+              ref={errRef}
+              className={errMsg ? "errMessage" : "noErrMessage"}
+            >
+              {errMsg.current}
+            </Typography>
+            <form>
+              <label htmlFor="title"></label>
+              <input
+                type="text"
+                name="title"
+                value={editedTitle}
+                onChange={(e) => setEditTitle(e.target.value)}
+              />
+
+              <label htmlFor="mood"></label>
+              <textarea
+                name="name"
+                rows={4}
+                value={editedMood}
+                onChange={(e) => setEditMood(e.target.value)}
+              ></textarea>
+
+              <button className="btn" onClick={editedPost}>
+                UPDATE
+              </button>
+            </form>
+          </section>
+        </Container>
+        {/* </>
               } */}
-              {/* {!editTitle && 
+        {/* {!editTitle &&
                   <>
                   <h6>Page not Found</h6>
                   <h3>Return to <span onClick={()=>navigate('/posts')}>POSTS</span></h3>
                   </>
               } */}
-          </div>
-     </Container>
-  )
-}
+      </div>
+    </Container>
+  );
+};
 
-export default EditPost
+export default EditPost;
--- frontend/moodboard/src/components/users/SingleUserPost.js
+++ frontend/moodboard/src/components/users/SingleUserPost.js
@@ -1,91 +1,88 @@
-import React, { useEffect, useState } from 'react'
-import { Container, Card, Grid, Button } from '@material-ui/core'
-import {styled} from '@material-ui/core/styles'
-import axios from 'axios'
-import { Link, useNavigate, useParams} from 'react-router-dom'
-import './SingleUserPost.css'
+import React, { useEffect, useState } from "react";
+import { Container, Card, Grid, Button } from "@material-ui/core";
+import { styled } from "@material-ui/core/styles";
+import axios from "axios";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import "./SingleUserPost.css";
 
 const SingleUserPost = () => {
-    
-    let { id } = useParams()
-    const navigate = useNavigate()
+  let { id } = useParams();
+  const navigate = useNavigate();
 
-    const [feed, setFeed] = useState([])
-    const [editTitle, setEditTitle] = useState('')
-    const [editMood, setEditMood] = useState('')
+  const [feed, setFeed] = useState([]);
+  const [editTitle, setEditTitle] = useState("");
+  const [editMood, setEditMood] = useState("");
 
-    useEffect(() => {
-        const getSinglePosts = async () => {
-            await axios
-                .get(`http://localhost:4000/post/${id}`)
-                .then((response) => {
-                    setFeed(response.data)
-                })
-                .catch((err) => {
-                    console.log(err);
-                })
-        };
-        getSinglePosts();
+  useEffect(() => {
+    const getSinglePosts = async () => {
+      await axios
+        .get(`http://localhost:4000/post/${id}`)
+        .then((response) => {
+          setFeed(response.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    getSinglePosts();
+  }, [feed]);
 
-    }, [feed])
+  const MyButton = styled(Button)({
+    background: "rgb(53, 66, 66)",
+    margin: 0,
+    color: "white",
+    width: "28px",
+    fontSize: "20px",
+    padding: ".5em 1.5em",
+    cursor: "pointer",
+  });
 
-    const MyButton = styled(Button)({
-        background: 'rgb(53, 66, 66)',
-        margin: 0,
-        color: 'white',
-        width: '28px',
-        fontSize: '20px',
-        padding: '.5em 1.5em',
-        cursor: 'pointer'
-    })
+  const deletePost = async () => {
+    await axios
+      .delete(`http://localhost:4000/post/${id}`)
+      .then((response) => {
+        setFeed(response.data);
+        navigate("/posts");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const deletePost = async () => {
-        await axios.delete(`http://localhost:4000/post/${id}`)
-            .then((response) => {
-                setFeed(response.data)
-                navigate('/posts')
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    }
+  // const editPost = async (id) => {
+  //     console.log(id);
+  //     try {
+  //         const updatedPost = await axios.put(`http://localhost:4000/post/${id}`, {
+  //         title: editTitle,
+  //         mood: editMood
+  //         })
+  //         setFeed(feed.map(post => feed.id === id ? { ...updatedPost.data } : feed))
+  //         navigate(`/posts/editpost/${id}`)
+  //     } catch (error) {
+  //         console.log(error);
+  //     }
+  // }
 
-    // const editPost = async (id) => {
-    //     console.log(id);
-    //     try {
-    //         const updatedPost = await axios.put(`http://localhost:4000/post/${id}`, {
-    //         title: editTitle,
-    //         mood: editMood
-    //         })
-    //         setFeed(feed.map(post => feed.id === id ? { ...updatedPost.data } : feed))  
-    //         navigate(`/posts/editpost/${id}`)
-    //     } catch (error) {
-    //         console.log(error);   
-    //     }
-    // }
-    
-
-    return (
-        <Container disableGutters maxWidth={false} className='primary'>
-            <Grid
-                className='mood-grid'
-                item
-                xs={12}>
-                <Card >
-                    <div className='single-user'>
-                        <div className='mood-key' key={feed.id}>
-                                <h3 className='poster'>{feed.name}</h3>
-                                <h4 className='mood-topic'>{feed.title}</h4>
-                                <h4 className='mood-body'>{feed.mood}</h4>
-                        </div>
-                        <div className='btn-div'>
-                            <MyButton onClick={()=> navigate(`/posts/editpost/${id}`)}>Edit</MyButton>
-                            <MyButton onClick={deletePost}>Delete</MyButton>
-                        </div>
-                    </div>    
-                </Card>
-            </Grid>
-        </Container>
-    )
-}
-export default SingleUserPost
+  return (
+    <Container disableGutters maxWidth={false} className="primary">
+      <Grid className="mood-grid" item xs={12}>
+        <Card>
+          <div className="single-user">
+            <div className="mood-key" key={feed.id}>
+              <h3 className="poster">{feed.name}</h3>
+              <h4 className="mood-topic">{feed.title}</h4>
+              <h4 className="mood-body">{feed.mood}</h4>
+            </div>
+            <div className="btn-div">
+              <button onClick={() => navigate(`/posts/editpost/${id}`)}>
+                Edit
+              </button>
+              <MyButton onClick={deletePost}>Delete</MyButton>
+            </div>
+          </div>
+        </Card>
+      </Grid>
+    </Container>
+  );
+};
+export default SingleUserPost;
